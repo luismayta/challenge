ca81addf386c7bb8f08c3c305220a0d002f58f2d
@slovacus <slovacus@gmail.com>
2019-10-29 16:06:34 -0500

    (#1) chore: implement actions for make

---
+++ provision/make/docs.mk
@@ -0,0 +1,17 @@
+#
+# See ./CONTRIBUTING.rst
+#
+
+FILE_README=$(ROOT_DIR)/README.rst
+
+docs:
+	make docs.help
+
+docs.help:
+	@echo '    Docs:'
+	@echo ''
+	@echo '        docs.show                  Show restview README'
+	@echo ''
+
+docs.show: clean
+	$(PIPENV_RUN) restview ${FILE_README}
