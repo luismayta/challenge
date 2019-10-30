07f018364d0106fb863be8e8f807ba687bc67a20
@slovacus <slovacus@gmail.com>
2019-10-30 18:39:16 -0500

    (#5) chore: enabled products router

--- frontend/backend/index.js
+++ frontend/backend/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRouter = require("./router/auth");
 const companiesRouter = require("./router/companies");
-// const productsRouter = require("./router/products");
+const productsRouter = require("./router/products");
 const usersRouter = require("./router/users");
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/auth", authRouter);
 app.use("/companies", companiesRouter);
-// app.use("/products", productsRouter);
+app.use("/products", productsRouter);
 app.use("/users", usersRouter);
 
 app.listen(3000, () => {
